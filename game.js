8deb92d17253a34bc2b1b364086224988dde3b9e
markodarko <markmcd26@gmail.com>
2020-06-03 07:38:47 -0400

    damage taking animation

--- game.js
+++ game.js
@@ -6,14 +6,50 @@ canvas.height = 640//window.innerHeight;
 
 const GRID = Math.floor(canvas.width/5)
 
+class Flash{
+	constructor(a = 0, inc = .05){
+			this.alpha = a;
+			this.inc = inc;
+	}
+	draw(x,y,w,h){
+		if (this.alpha == 0)return
+		ctx.save();
+		ctx.globalAlpha = this.alpha
+		ctx.fillRect(x,y,w,h)
+		this.alpha = Math.max(this.alpha - this.inc, 0)
+		ctx.restore();
+	}
+}
+class FlashLoop extends Flash{
+	constructor(a,inc){
+		super(a,inc);
+	}
+	draw(x,y,w,h){
+		if (this.alpha == 0) {
+			this.inc *= -1;
+			this.alpha -= this.inc;
+		}
+		if (this.alpha >= 1) {
+			this.alpha = 1;
+			this.inc *= -1;
+		}
+		super.draw(x,y,w,h);
+	}
+}
+
 class EnemySquare{
-	constructor(size){
+	constructor(size,color){
+		this.color = color;
 		this.dead = false;
 		this.setPosition()
 		this.w = this.h = size;
+		this.flash = new FlashLoop(Math.random(),.02);
 	}
 	draw(){
+		ctx.fillStyle = '#e45c10'
 		ctx.fillRect(this.x,this.y,this.w,this.h);
+		ctx.fillStyle = '#e40058'
+		this.flash.draw(this.x,this.y,this.w,this.h);
 	}
 	setPosition(){
 		this.score = 300;
@@ -30,7 +66,7 @@ class EnemySpawner{
 		this.liveEnemies = [];
 		this.deadEnemies = [];
 		for (let i=0; i<10; i++){
-			this.deadEnemies.push(new EnemySquare(GRID))
+			this.deadEnemies.push(new EnemySquare(GRID,'#e45c10'))
 		}
 	}
 	update(){
@@ -40,6 +76,7 @@ class EnemySpawner{
 			this.liveEnemies.unshift(this.deadEnemies.shift())
 			}
 		}
+		var hitEnemy = false;
 		for(let i = this.liveEnemies.length-1; i>=0; i--){
 			let enemy = this.liveEnemies[i];
 			enemy.y -= this.speed;
@@ -53,17 +90,17 @@ class EnemySpawner{
 				enemy.dead = true;
 				continue;
 			}
-			if (touchX == null) continue;
-			if (touchX < enemy.x || touchX > enemy.x + enemy.w) continue;
-			if (touchY < enemy.y || touchY > enemy.y + enemy.h) continue;
-				enemy.dead = true;
-				touchX = touchY = null;
+			if (TouchControls.touchX == null) continue;
+			var X = TouchControls.touchX, Y = TouchControls.touchY;
+			if (X < enemy.x || X > enemy.x + enemy.w) continue;
+			if (Y < enemy.y || Y > enemy.y + enemy.h) continue;
+			enemy.dead = true;
+			hitEnemy = true;
 		}
 		this.time++;
+		if (hitEnemy == false && TouchControls.touchX)GAME.takeDamage();
 	}
-		
 	draw(){
-		ctx.fillStyle = '#e45c10'
 		this.liveEnemies.forEach(enemy => {
 			enemy.draw();
 		})
@@ -73,8 +110,16 @@ class EnemySpawner{
 class GameControl{
 	constructor(){
 		this.score = 0;
+		this.flash = new Flash();
 		this.background = new GameBackground()
 		this.spawner = new EnemySpawner()
+	}
+	takeDamage(){
+		this.flash.alpha = 1;
+	}
+	drawFlash(){
+		ctx.fillStyle = '#e40058'
+		this.flash.draw(0,0,canvas.width,canvas.height)
 	}
 }
 
@@ -84,22 +129,31 @@ function loop(){
 	ctx.clearRect(0,0,canvas.width,canvas.height)
 	GAME.background.draw();
 	GAME.spawner.update();
+	GAME.drawFlash();
 	GAME.spawner.draw();
-	touchY = null;
-	touchX = null;
+	TouchControls.clearControl();
 }
 
-var touchX = null, touchY = null;
-window.addEventListener('touchstart',(e)=>{
-	touchX = e.changedTouches[0].clientX;
-	touchY = e.changedTouches[0].clientY;
-	GAME.background.starfields[0].maxSpeed = 40;
-	GAME.background.starfields[1].maxSpeed = 30;
-})
-window.addEventListener('touchend', (e)=>{
-	GAME.background.starfields[0].maxSpeed = 3;
-	GAME.background.starfields[1].maxSpeed = 1;
-})
+var TouchControls = {
+	touchX:null,
+	touchY:null,
+	clearControl: function(){
+		this.touchX = null;
+		this.touchY = null;
+	},
+	tap:function(e){
+		TouchControls.touchX = e.changedTouches[0].clientX;
+		TouchControls.touchY = e.changedTouches[0].clientY;
+		GAME.background.starfields[0].maxSpeed = 40;
+		GAME.background.starfields[1].maxSpeed = 30;
+	},
+	release:function(e){
+		GAME.background.starfields[0].maxSpeed = 3;
+		GAME.background.starfields[1].maxSpeed = 1;
+	}
+}
+window.addEventListener('touchstart',TouchControls.tap)
+window.addEventListener('touchend', TouchControls.release)
 
 var GAME = new GameControl();
 loop();
